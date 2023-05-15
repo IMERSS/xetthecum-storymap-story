efa70cb78f90f238063df476558787bfca503ef2
Antranig Basman <antranig.basman@colorado.edu>
2023-05-15 13:47:25 +0100

    Fully working version of coordinate rounding and simplification

--- scripts/utils.R
+++ scripts/utils.R
@@ -4,28 +4,44 @@ lat_lon <- function (data) {
   return (st_transform(data, "+proj=longlat +datum=WGS84"))
 }
 
-
-roundmulti <- function (multi) {
+roundmulti <- function (multi, digits) {
   multi <- lapply(multi, function (matrix) {
     matrix <- lapply(matrix, function (coords) {
-      round(coords, 4)
+      round(coords, digits)
     })
   })
   return (st_multipolygon(multi))
 }
 
-roundgeom <- function (geom) {
-  geom <- lapply(geom, roundmulti)
-  return (st_sfc(geom))
+roundpoly <- function (poly, digits) {
+  poly <- lapply(poly, function (matrix) {
+      round(matrix, digits)
+  })
+  return (st_polygon(poly))
 }
 
-mx_read <- function (filename) {
+round_sf <- function (fc, digits) {
+  # https://gis.stackexchange.com/questions/329110/removing-empty-polygon-from-sf-object-in-r
+  simple  <- fc %>% st_simplify(preserveTopology = TRUE, dTolerance = 5) %>% filter(!st_is_empty(.))
+  geom <- simple$geometry
+  geom <- lapply(geom, function (one) {
+    if (inherits(one, "MULTIPOLYGON")) {
+      one <- roundmulti(one, digits)
+    } else if (inherits(one, "POLYGON")) {
+      one <- roundpoly(one, digits)
+    } else if (!st_is_empty(one)) {
+      stop(paste("I don't know what it is ", class(one)))  
+    }
+  })
+  simple$geometry <- st_sfc(geom)
+  simple
+}
+
+mx_read <- function (filename, digits = 4) {
   st_data <- st_read(filename, quiet=TRUE);
   dropped <- st_zm(st_data, drop = T, what = "ZM")
   trans <- lat_lon(dropped);
-  geom <- trans$geometry;
-  trans$geometry <- roundgeom(geom)
-  return(trans)
+  rounded <- round_sf(trans, digits);
 }
 
 # Attach the region's label as an "mx_regionId" option in the output data
