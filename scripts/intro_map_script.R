6ae35a36ea7144d1d0df58f24ca32463b039d6bb
Tertiant Studios <33681780+Tertiant@users.noreply.github.com>
2024-02-14 12:34:39 -0800

    Updated Greig Creek to render correctly (fixed it rendering as a polgyon) and added a stroke to each polygon layer.

--- scripts/intro_map_script.R
+++ scripts/intro_map_script.R
@@ -12,7 +12,9 @@ library(stringr)
 source("scripts/utils.R")
 
 # import datasets
-layerStyling <- timedFread("tabular_data/layerStyling.csv")
+polygonStyling <- timedFread("tabular_data/polygonStyling.csv")
+lineStyling <- timedFread("tabular_data/lineStyling.csv")
+
 
 mx_intro_map <- function () {
   title <- "Xetthecum Introduction";
@@ -22,13 +24,26 @@ mx_intro_map <- function () {
     fitBounds(-123.51834, 48.93672,-123.49091, 48.95141) %>%
     addProviderTiles(providers$CartoDB.Positron)
     
-    for (i in 1:nrow(layerStyling)) {
-      row <- layerStyling[i,]
+    for (i in 1:nrow(polygonStyling)) {
+      row <- polygonStyling[i,]
       introMap <- introMap %>% addPolygons(data = mx_read(paste("spatial_data/vectors/",row$Layer, sep="")), 
                                           fillColor = row$fillColor, 
                                           fillOpacity = as.numeric(row$fillOpacity), 
-                                          weight = 0.5,
+                                          stroke=TRUE,
+                                          color=row$outlineColor,
+                                          opacity=row$outlineOpacity,
+                                          weight = row$outlineWidth,
                                           options = list(mx_layerId = row$Layer, className = row$ClassName))
+    }
+  
+    for (i in 1:nrow(lineStyling)) {
+      row <- lineStyling[i,]
+      introMap <- introMap %>% addPolylines(data = mx_read(paste("spatial_data/vectors/",row$Layer, sep="")), 
+                                           stroke=TRUE,
+                                           color=row$outlineColor,
+                                           opacity=as.numeric(row$outlineOpacity),
+                                           weight = as.numeric(row$outlineWidth),
+                                           options = list(mx_layerId = row$Layer, className = row$ClassName))
     }
   
   #Note that this statement is only effective in standalone R
