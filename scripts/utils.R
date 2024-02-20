86578d2ee942630623dba8bc4fd97abbba49e6de
Antranig Basman <antranig.basman@colorado.edu>
2024-02-20 13:23:53 +0000

    Moved away from "standalone" build of R markdown, repaired coordinate rounding to handle multilinestring, removed surplus scrollbars, embedded videos now work, filesize is 20x smaller

--- scripts/utils.R
+++ scripts/utils.R
@@ -1,16 +1,26 @@
 library(sf)
 
+# Attested at https://stackoverflow.com/questions/68478179/how-to-resolve-spherical-geometry-failures-when-joining-spatial-data
+sf::sf_use_s2(FALSE)
+
 lat_lon <- function (data) {
   return (st_transform(data, "+proj=longlat +datum=WGS84"))
 }
 
-roundmulti <- function (multi, digits) {
+roundmultipolygon <- function (multi, digits) {
   multi <- lapply(multi, function (matrix) {
     matrix <- lapply(matrix, function (coords) {
       round(coords, digits)
     })
   })
   return (st_multipolygon(multi))
+}
+
+roundmultilinestring <- function (poly, digits) {
+  poly <- lapply(poly, function (matrix) {
+    round(matrix, digits)
+  })
+  return (st_multilinestring(poly))
 }
 
 roundpoly <- function (poly, digits) {
@@ -22,11 +32,14 @@ roundpoly <- function (poly, digits) {
 
 round_sf <- function (fc, digits) {
   # https://gis.stackexchange.com/questions/329110/removing-empty-polygon-from-sf-object-in-r
-  simple  <- fc %>% st_simplify(preserveTopology = TRUE, dTolerance = 5) %>% dplyr::filter(!st_is_empty(.))
+  #simple  <- fc %>% st_simplify(preserveTopology = TRUE, dTolerance = 0.5) %>% dplyr::filter(!st_is_empty(.))
+  simple <- fc;
   geom <- simple$geometry
-  geom <- lapply(geom, function (one) {
+  rgeom <- lapply(geom, function (one) {
     if (inherits(one, "MULTIPOLYGON")) {
-      one <- roundmulti(one, digits)
+      one <- roundmultipolygon(one, digits)
+    } else if (inherits(one, "MULTILINESTRING")) {
+      one <- roundmultilinestring(one, digits)
     } else if (inherits(one, "POLYGON")) {
       one <- roundpoly(one, digits)
     } else if (inherits(one, "XY")) {
@@ -35,15 +48,15 @@ round_sf <- function (fc, digits) {
       stop(paste("I don't know what it is ", class(one)))
     }
   })
-  simple$geometry <- st_sfc(geom)
+  simple$geometry <- st_sfc(rgeom)
   simple
 }
 
-mx_read <- function (filename, digits = 4) {
+mx_read <- function (filename, digits = 5) {
   st_data <- st_read(filename, quiet=TRUE);
   dropped <- st_zm(st_data, drop = T, what = "ZM")
   trans <- lat_lon(dropped);
-  # rounded <- round_sf(trans, digits);
+  rounded <- round_sf(trans, digits);
 }
 
 # Attach the region's label as an "mx_regionId" option in the output data
